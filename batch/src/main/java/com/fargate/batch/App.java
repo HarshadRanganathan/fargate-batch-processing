ca2c08e242407439fa0a9038930530823d9e080b
RanganathanH <ranganathanh@dnb.com>
2019-04-16 13:00:52 +0100

    Initial project structure

---
+++ batch/src/main/java/com/fargate/batch/App.java
@@ -0,0 +1,13 @@
+package com.fargate.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
